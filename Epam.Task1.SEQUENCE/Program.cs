921a0aa163b6d26f37320695c557443406086600
Eduard_Sagetdinov <sagetdinoveduard@gmail.com>
2018-11-18 15:21:08 +0300

    Task 1

---
+++ Epam.Task1.SEQUENCE/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*Написать функцию, формирующую и возвращающую строку вида 1, 2, 3, … N (положительное число). 
+ * Значение N передаётся в функцию в качестве аргумента. Пример возвращаемого значения для N=7: 1, 2, 3, 4, 5, 6, 7 */
+namespace Epam.Task1.SEQUENCE
+{
+    class Program
+    {
+        public  static void ShowNumbers(int n)
+        {
+            for(int i=1; i<n; i++)
+            {
+                Console.Write(i+", ");
+            }
+            Console.WriteLine(n);
+        }
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Enter some positive number: ");
+            if((!int.TryParse(Console.ReadLine(), out n))||(n<=0))
+            {
+                Console.WriteLine("Enter some positive number, please!!!");
+            }else
+            {
+                ShowNumbers(n);
+            }
+         
+           
+        }
+    }
+}
---
+++ Epam.Task1.SIMPLE/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* Число считается простым, если его можно разделить без остатка только на единицу
+ и на само себя (например, 7). Необходимо написать функцию, определяющую, является ли 
+ заданное число N (положительное целое) простым. Значение N передаётся в функцию в качестве аргумента. */
+namespace Epam.Task1.SIMPLE
+{
+    class Program
+    {
+        static bool IsPrime(int n)
+        {
+            if (n <= 2)
+                return false;
+            for(int i=2; i<=Math.Sqrt(n); i++)
+            {
+                if (n % i == 0)
+                    return false;
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Enter some positive number: ");
+            if (!int.TryParse(Console.ReadLine(), out n))
+            {
+                Console.WriteLine("Enter some positive number, please!!!");
+                return;
+            }
+            if(IsPrime(n))
+            {
+                Console.WriteLine("Your number is prime!");
+            }else
+            {
+                Console.WriteLine("Your number is not prime!");
+            }
+        }
+    }
+}
