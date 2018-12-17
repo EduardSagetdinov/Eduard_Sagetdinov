6c2d87f69964e915916e1cbf126193b91fbd8661
Сагетдинов Эдуард <42378998+EduardSagetdinov@users.noreply.github.com>
2018-12-17 08:21:04 +0400

    Readded Task3.User

---
+++ Epam.Task3/Epam.Task3.USER/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task3.USER
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            User us;
+            string sn;
+            string n;
+            string patr;
+            string d;
+            Console.Write("What is your surname: ");
+            sn = Console.ReadLine();
+            Console.Write("What is your name: ");
+            n = Console.ReadLine();
+            Console.Write("What is your patronymic: ");
+            patr = Console.ReadLine();
+            Console.Write("What is your day of birth? Only day, please:  ");
+            d = Console.ReadLine();
+            d += " ";
+            Console.Write("What is your month of birth? Only number of month, please: ");
+            d += Console.ReadLine();
+            d += " ";
+            Console.Write("What is your year of birth? Four digits, please: ");
+            d += Console.ReadLine();
+            try
+            {
+                us = new User(sn, n, patr, d);
+                if (us.Age >= 150 || us.Age <= 0)
+                {
+                    Console.WriteLine("It is not your date of birth!");
+                }
+                else
+                {
+                    Console.WriteLine($"Hello, {us.Surname} {us.Name} {us.Patronymic}!!!");
+                    Console.WriteLine($"You date of birth is: {us.Birth.ToString("dd-MM-yyyy")}");
+                    Console.WriteLine($"You age is {us.Age} full yeas.");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message); 
+            }
+        }
+    }
+}
---
+++ Epam.Task3/Epam.Task3.USER/User.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task3.USER
+{
+    public class User
+    {
+        private readonly DateTime now = DateTime.Now;
+
+        public User(string sn, string n, string patr, string db)
+        {
+                this.Test(ref sn);
+                this.Test(ref n);
+                this.Test(ref patr);
+                this.Test(db, out DateTime d);
+                this.Surname = sn;
+                this.Name = n;
+                this.Patronymic = patr;
+                this.Birth = d;
+                this.Age = (this.now.Month > this.Birth.Month)
+                ? (this.now.Year - this.Birth.Year) : (this.now.Year - this.Birth.Year - 1);
+        }
+
+        public string Surname { get; set; }
+
+        public string Name { get; set; }
+
+        public string Patronymic { get; set; }
+
+        public DateTime Birth { get; set; }
+
+        public int Age { get; set; }
+
+        public void Test(ref string s)
+        {
+            s = s.Trim();
+            s = s.Replace("\t", string.Empty);
+            var sb = new StringBuilder(s);
+            sb[0] = char.ToUpper(sb[0]);
+            s = sb.ToString();
+            foreach (var item in s)
+            {
+                if (!char.IsLetter(item))
+                {
+                    throw new Exception("Wrong data!");
+                }
+            }
+        }
+
+        public void Test(string db, out DateTime d)
+        {
+            if (!DateTime.TryParse(db, out d))
+            {
+                throw new Exception("Wrong date format!");
+            }
+        }
+    }
+}
